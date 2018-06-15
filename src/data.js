2e34d25453263eb4c354d53e46e24ece368aee1a
viviana <dmpviviana@gmail.com>
2018-06-15 13:19:48 -0400

    arreglo para recorrer las 3 datas

--- src/data.js
+++ src/data.js
@@ -1,4 +1,19 @@
 fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json').then((usersResponse) =>{
   return usersResponse.json();
-}).then();
+}).then(users => {
+  console.log(JSON.stringify(users[0].name));
+});
+const llamadas = [];
+llamadas.push(fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'));
+llamadas.push(fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'));
+llamadas.push(fetch('../data/cohorts.json'));
+
+Promise.all(llamadas).then((responses)=>{
+    return responses.map(response => response.json()); 
+    //En caso de que sean llamadas a api
+}).then((jsonResponses)=>{
+   console.log(jsonResponses)
+}).catch((error)=>{
+    //Código que maneja errores
+});
 // con fecth
--- src/main.js
+++ src/main.js
@@ -1,28 +1,46 @@
-window.onload = function() {
-  getNews();
-};
+// window.onload = function() {
+//   getNews();
+// };
 
-function getNews() { // q=${searchForText}
-  const articleRequest = new XMLHttpRequest();
-  articleRequest.open('GET', '../data/cohorts.json/', true);
-  articleRequest.onload = addNews;
-  articleRequest.onerror = handleError;
-  articleRequest.send();
-  function addNew() {
-    const userData = JSON.parse(users.responseText);
-    console.log(userData);
-    console.log(Object.entries(userData));
-  }
-  const users = new XMLHttpRequest();
-  users.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/users.json', true);
-  users.onload = addNew;
-  users.onerror = handleError;
-  users.send();
-}
-function addNews() {
-  const data = JSON.parse(this.responseText);
-  console.log(data);
-}
-function handleError() {
-  console.log('se ha presentado un error');
-}
+// function getNews() { // q=${searchForText}
+//   const articleRequest = new XMLHttpRequest();
+//   articleRequest.open('GET', '../data/cohorts.json/', true);
+//   articleRequest.onload = addNews;
+//   articleRequest.onerror = handleError;
+//   articleRequest.send();
+//   function addNews() {
+//     const data = JSON.parse(this.responseText);
+//     console.log(data);
+//   }
+
+
+//   const users = new XMLHttpRequest();
+//   users.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/users.json', true);
+//   users.onload = addNew;
+//   users.onerror = handleError;
+//   users.send();
+
+//   function addNew() {
+//     const userData = JSON.parse(users.responseText);
+//     console.log(userData);
+//   }
+// }
+
+// function handleError() {
+//   console.log('se ha presentado un error');
+// }
+
+const llamadas = [];
+llamadas.push(fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'));
+llamadas.push(fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'));
+llamadas.push(fetch('../data/cohorts.json'));
+
+Promise.all(llamadas).then((responses)=>{
+    return responses.map(response => response.json()); 
+    //En caso de que sean llamadas a api
+}).then((jsonResponses)=>{
+   console.log(jsonResponses)
+}).catch((error)=>{
+    //Código que maneja errores
+});
+// con fecth
