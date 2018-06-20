60bae24b97ae843d32968bed3d4fa1b1cfbe90c7
SofiaMaturanaM <sofimatsu@gmail.com>
2018-06-20 14:10:15 -0400

    conflictos data.js

--- src/data.js
+++ src/data.js
@@ -1,48 +1,32 @@
 Promise.all([ // Ejecuta todas las llamadas de manera paralela
-  fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
-  fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
-  fetch('../data/cohorts.json')
+    fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
+    fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
+    fetch('../data/cohorts.json')
 ]).then(
-  (responses)=>{ // Responde a todas las promesas
-    return Promise.all(responses.map((response)=>{
-      return response.json();
-    }));
-  }
-).then((responseJsons)=>{ // Arreglo de respuestas en json
-  console.log(responseJsons);
-  computeUsersStats(responseJsons[0], responseJsons[1], responseJsons[2]);
-  //
-  // Código que ocupa los jsons...
-  //
+    (responses) => { // Responde a todas las promesas
+        return Promise.all(responses.map((response) => {
+            return response.json();
+        }));
+    }
+).then((responseJsons) => { // Arreglo de respuestas en json
+    console.log(responseJsons);
+    computeUsersStats(responseJsons[0], responseJsons[1], responseJsons[2]);
+    //
+    // Código que ocupa los jsons...
+    //
 }).catch(
-  
-  (error)=>{ // Al menos una llamada falló
-    console.log(error);
-  }
+
+    (error) => { // Al menos una llamada falló
+        console.log(error);
+    }
 );
 
-function computeUsersStats(users, progress, courses) {
-  // console.log(users);
-  // console.log(progress);
-  // console.log(courses);
-  const usu = users.map(usua => {
-    console.log(usua.signupCohort === 'lim-2018-03-pre-core-pw');
-  });
-  console.log(usu);
-  const usuarios = users.find(item => item.signupCohort === 'lim-2018-03-pre-core-pw');
-  const cohort = courses.find(item => item.id === 'lim-2018-03-pre-core-pw');
-  const coursesLim = Object.keys(cohort.coursesIndex);
-  console.log(cohort);
-  console.log(usuarios);
-}
-
-function sortUsers(users, orderBy, orderDirection) {
-
-}
-
-function filterUsers(users, search) {
-}
-
-function processCohortData(options) {
-
-}
+Promise.all(llamadas).then((responses) => {
+    return responses.map(response => response.json());
+    //En caso de que sean llamadas a api
+}).then((jsonResponses) => {
+    console.log(jsonResponses)
+}).catch((error) => {
+    //Código que maneja errores
+});
+// con fetch
