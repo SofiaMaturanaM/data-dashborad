b42e18d13e032ce1d22525e45638886655720e97
viviana <dmpviviana@gmail.com>
2018-06-21 17:51:52 -0400

    corhorts

--- src/data.js
+++ src/data.js
@@ -1,36 +1,36 @@
 Promise.all([ // Ejecuta todas las llamadas de manera paralela
-    fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
-    fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
-    fetch('../data/cohorts.json')
+  fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
+  fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
+  fetch('../data/cohorts.json')
 ]).then(
-    (responses) => { // Responde a todas las promesas
-        return Promise.all(responses.map((response) => {
-            return response.json();
-        }));
-    }
+  (responses) => { // Responde a todas las promesas
+    return Promise.all(responses.map((response) => {
+      return response.json();
+    }));
+  }
 ).then((responseJsons) => { // Arreglo de respuestas en json
-    console.log(responseJsons);
-    computeUsersStats(responseJsons[0], responseJsons[1], responseJsons[2]);
-    //
-    // Código que ocupa los jsons...
-    //
+  console.log(responseJsons);
+  computeUsersStats(responseJsons[0], responseJsons[1], responseJsons[2]);
+  //
+  // Código que ocupa los jsons...
+  //
 }).catch(
 
-    (error) => { // Al menos una llamada falló
-        console.log(error);
-    }
+  (error) => { // Al menos una llamada falló
+    console.log(error);
+  }
 );
 
 function computeUsersStats(users, progress, courses) {
   // console.log(users);
-  let container = document.getElementById('contenedor');
+  // let container = document.getElementById('contenedor');
   console.log(progress);
 
-// const cursos =  courses.map(ele => {
-//   console.log(e.id)
-// })
+  const cursos = courses.map(ele => {
+    return ele.id;
+  });
 
-  // console.log(courses);
+  console.log(cursos);
   const usu = users.map(usua => {
     return {
       id: usua.id,
@@ -39,7 +39,7 @@ function computeUsersStats(users, progress, courses) {
     };
   });
   const objetoComoTexto = JSON.stringify(usu);
-  container.innerHTML = objetoComoTexto;
+  // container.innerHTML = objetoComoTexto;
   const progreso = Object.entries(progress);
   progreso.forEach(element => {
     console.log(element[1].intro.percent);
@@ -71,14 +71,13 @@ function filterUsers(users, search) {
 }
 
 function processCohortData(options) {
-
-    Promise.all(llamadas).then((responses) => {
-        return responses.map(response => response.json());
-        //En caso de que sean llamadas a api
-    }).then((jsonResponses) => {
-        console.log(jsonResponses)
-    }).catch((error) => {
-        //Código que maneja errores
-    });
+  Promise.all(llamadas).then((responses) => {
+    return responses.map(response => response.json());
+    // En caso de que sean llamadas a api
+  }).then((jsonResponses) => {
+    console.log(jsonResponses);
+  }).catch((error) => {
+    // Código que maneja errores
+  });
 }
 // con fetch
