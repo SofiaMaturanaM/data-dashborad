07ba31a33aca202fc8531f7d796e7b96f6aa7cfc
viviana <dmpviviana@gmail.com>
2018-06-22 18:30:41 -0400

    trabajo en quipo live share

--- src/data.js
+++ src/data.js
@@ -1,65 +1,68 @@
 Promise.all([ // Ejecuta todas las llamadas de manera paralela
-  fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
-  fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
-  fetch('../data/cohorts.json')
+    fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
+    fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
+    fetch('../data/cohorts.json')
 ]).then(
-  (responses) => { // Responde a todas las promesas
-    return Promise.all(responses.map((response) => {
-      return response.json();
-    }));
-  }
+    (responses) => { // Responde a todas las promesas
+        return Promise.all(responses.map((response) => {
+            return response.json();
+        }));
+    }
 ).then((responseJsons) => { // Arreglo de respuestas en json
-  console.log(responseJsons);
-  computeUsersStats(responseJsons[0], responseJsons[1], responseJsons[2]);
-  //
-  // Código que ocupa los jsons...
-  //
+    console.log(responseJsons);
+    let progress = Object.entries(responseJsons[1]);
+
+    progress.forEach(element => {
+        console.log(Object.entries(element[1]));
+    });
+    dropdown1(responseJsons[2]);
+    computeUsersStats(responseJsons[0], progress, responseJsons[2]);
+    //
+    // Código que ocupa los jsons...
+    //
 }).catch(
 
-  (error) => { // Al menos una llamada falló
-    console.log(error);
-  }
+    (error) => { // Al menos una llamada falló
+        console.log(error);
+    }
 );
 
 function computeUsersStats(users, progress, courses) {
-  // console.log(users);
-  // let container = document.getElementById('contenedor');
-  console.log(progress);
 
-  const cursos = courses.map(ele => {
-    return ele.id;
-  });
-
-  console.log(cursos);
-  const usu = users.map(usua => {
-    return {
-      id: usua.id,
-      name: usua.name,
-      signupCohort: usua.signupCohort,
-    };
-  });
-  const objetoComoTexto = JSON.stringify(usu);
-  // container.innerHTML = objetoComoTexto;
-  const progreso = Object.entries(progress);
-  progreso.forEach(element => {
-    console.log(element[1].intro.percent);
-  });
-  // let hi = progreso.map(element => {
-  //   // console.log(element[1].intro.percent);
-  //   return Object.keys(element)
-  // });
-  // console.log(hi);
-  
-  console.log(progressUser);
-  console.log(progreso);
-  
-
-  const usuarios = users.find(item => item.signupCohort === 'lim-2018-03-pre-core-pw');
-  const cohort = courses.find(item => item.id === 'lim-2018-03-pre-core-pw');
-  const coursesLim = Object.keys(cohort.coursesIndex);
-  // console.log(cohort);
-  // console.log(usuarios);
+    // console.log(progress);
+    let user = users.map(element => {
+        return {
+            id: element.id,
+            name: element.name,
+            stats: {
+                percent: 0,
+                exercises: {
+                    total: 0,
+                    completed: 0,
+                    percent: 0
+                },
+                reads: {
+                    total: 0,
+                    completed: 0,
+                    percent: 0
+                },
+                quizzes: {
+                    total: 0,
+                    completed: 0,
+                    percent: 0,
+                    scoreSum: 0,
+                    scoreAvg: 0,
+                },
+            }
+        };
+    });
+    // console.log(user);
 }
+
+function percent() {
+
+}
+
 
 function sortUsers(users, orderBy, orderDirection) {
 
@@ -67,17 +70,51 @@ function sortUsers(users, orderBy, orderDirection) {
 }
 
 function filterUsers(users, search) {
+    // acá hay que llamar al click del botón de filtrado!!!! 
 
 }
 
-function processCohortData(options) {
-  Promise.all(llamadas).then((responses) => {
-    return responses.map(response => response.json());
-    // En caso de que sean llamadas a api
-  }).then((jsonResponses) => {
-    console.log(jsonResponses);
-  }).catch((error) => {
-    // Código que maneja errores
-  });
-}
+
 // con fetch
+
+
+// console.log(users);
+// let container = document.getElementById('contenedor');
+// console.log(progress);
+
+// const cursos = courses.map(ele => {
+//   return ele.id;
+// });
+
+// console.log(cursos);
+// const usu = users.map(user => {
+//  return progress[user.id].length === 1;
+
+//   // return {
+//   //   id: usua.id,
+//   //   name: usua.name,
+//   //   signupCohort: usua.signupCohort,
+//   // };
+// });
+// console.log(usu);
+// const objetoComoTexto = JSON.stringify(usu);
+// // container.innerHTML = objetoComoTexto;
+// const progreso = Object.entries(progress);
+// progreso.forEach(element => {
+//   console.log(element[1].intro.percent);
+// });
+// // let hi = progreso.map(element => {
+// //   // console.log(element[1].intro.percent);
+// //   return Object.keys(element)
+// // });
+// // console.log(hi);
+
+// console.log(progressUser);
+// console.log(progreso);
+
+
+// const usuarios = users.find(item => item.signupCohort === 'lim-2018-03-pre-core-pw');
+// const cohort = courses.find(item => item.id === 'lim-2018-03-pre-core-pw');
+// const coursesLim = Object.keys(cohort.coursesIndex);
+// // console.log(cohort);
+// // console.log(usuarios);
