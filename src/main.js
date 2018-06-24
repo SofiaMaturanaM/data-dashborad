021bf7c7c7cb6770ffb28c4d37974135ffe6a1ef
viviana <dmpviviana@gmail.com>
2018-06-24 19:40:17 -0400

    filtrado de usuario de la tabla

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ function dropdown1(courses) {
     let btn = document.createElement('button');
     if (id === 'lim-2018-03-pre-core-pw') {
       btn.className = 'dropdown-item';
-      btn.setAttribute('onclick', 'cohort();')
+      btn.setAttribute('onclick', 'cohort();');
     } else {
       btn.className = 'dropdown-item disabled';
     };
@@ -63,5 +63,21 @@ function cohort(user) {
 //   console.log(user);
  
 // }
-
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  table = document.getElementById('myTable');
+  tr = table.getElementsByTagName('tr');
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName('td')[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = '';
+      } else {
+        tr[i].style.display = 'none';
+      }
+    }       
+  }
+}
   
